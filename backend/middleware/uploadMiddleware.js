d2db18772408cc9b471d0e7fd1823a00863c5550
Saanvi8888 <saanvigupta8888@gmail.com>
2026-01-30 22:36:44 +0530

    New change

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -4,38 +4,24 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dummy',
+  api_key: process.env.CLOUDINARY_API_KEY || 'dummy',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'dummy',
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'expense-tracker',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp'],
-    public_id: (req, file) => {
-      
-      const name = file.originalname.split('.')[0];
-      return `${Date.now()}-${name}`;
-    }
+    allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'jfif'],
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`
   },
 });
 
 
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true); 
-    } else {
-        cb(new Error("Only .jpeg, .jpg, and .png formats are allowed"), false);
-    }
-};
-
 const upload = multer({ 
-  storage, 
-  fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 } 
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }
 });
 
 module.exports = upload;
