84654fe754b83631e0759188c51d2cce0f32a945
Saanvi8888 <saanvigupta8888@gmail.com>
2026-01-30 20:00:05 +0530

    New change

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,7 @@
-export const BASE_URL="http://localhost:8000"
-
+// export const BASE_URL="http://localhost:8000"
+export const BASE_URL = import.meta.env.PROD 
+  ? ""  
+  : "http://localhost:8000";
 export const API_PATHS = {
     AUTH:{
         LOGIN:"/api/v1/auth/login",
